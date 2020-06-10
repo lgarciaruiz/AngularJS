7190ae803e962c86a4a0cbb6a3c5490213d75ef3
LGarciaRuiz <lgarciaruiz23@gmail.com>
2020-06-09 20:41:25 -0700

    Starting first angular app

---
+++ Intro-ModulesAppsControllers/app.js
@@ -0,0 +1,7 @@
+//angualr module takes in the name of the app and an array of dependencies; angular.module is an app
+var myApp = angular.module('myApp',[]);
+
+//always declare a controller; the first param is the name of the controller then a callback function for what this controller will do
+myApp.controller('mainController', function(){
+
+});
